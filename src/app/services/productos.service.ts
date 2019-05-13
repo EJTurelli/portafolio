326e71bce96bc1c33678d6f70cacc820360fb5e2
ejturelli <ejturelli@yahoo.com>
2019-05-13 14:03:35 -0300

    Fin de la sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,8 @@ export class ProductosService {
 
   productos: productosIdx[] = [];
   cargando = true;
+  
+  productoFiltrado: productosIdx[] = [];
 
   constructor( private http: HttpClient ) { 
 
@@ -18,11 +20,54 @@ export class ProductosService {
 
   private cargarProductos () {
 
-    this.http.get('https://angular-portfolio-d275a.firebaseio.com/productos_idx.json')
+    return new Promise ( (resolve, reject) => {
+
+      this.http.get('https://angular-portfolio-d275a.firebaseio.com/productos_idx.json')
       .subscribe( (resp: productosIdx[]) => {
         this.productos = resp;
         this.cargando = false;
-      });
+        resolve();
+      });      
 
+    });
+
+  }
+
+  getProducto (id: string) {
+    return this.http.get(`https://angular-portfolio-d275a.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto ( termino: string) {
+    
+    if ( this.productos.length === 0 ) {
+      // esperar
+      this.cargarProductos().then( ()=> {
+        // cuando ya se tengan los productos
+        // Aplicar filtro
+        this.filtrarProductos( termino );
+
+      });
+    }
+    else {
+      // filtrar
+      this.filtrarProductos( termino );
+    }
+
+  }
+
+  private filtrarProductos( termino: string ) {
+    this.productoFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach ( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      const categoriaLower = prod.categoria.toLocaleLowerCase();
+
+      if ( categoriaLower.indexOf(termino) >=0 || tituloLower.indexOf( termino ) >= 0) {
+        this.productoFiltrado.push( prod );
+      }
+
+    });
   }
 }
